c97c69cb8b8b69a27f3c63fbc4b296d15c2ea541
pf4 <74263128+elydre@users.noreply.github.com>
2023-07-16 12:13:47 +0200

    update

---
+++ generator/html/build.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+
+def get_json_data(path):
+    with open(path, "r") as f:
+        data = json.load(f)
+    return data
+
+def get_output_path(path):
+    return os.path.join("build", path) + ".html"
+
+def get_json_path(path):
+    return os.path.join("source", path) + ".json"
+
+def get_template():
+    with open("generator/html/template.html", "r") as f:
+        template = f.read()
+    return template
+
+def generate_repdict(data):
+    path_links = "".join(
+        f'<a href="/{l}.html">{l}</a>'
+        if i == len(data["doc_path"]) - 1
+        else f'<a href="/{l}">{l}</a> / '
+        for i, l in enumerate(data["doc_path"])
+    )
+
+    return {
+        "NAME": data["doc_name"],
+        "PATH_LINKS": path_links,
+    }
+
+def generate_html(template, repdict):
+    for key, value in repdict.items():
+        template = template.replace("{" + key + "}", value)
+    return template
+
+def write_html(path, html):
+    with open(path, "w") as f:
+        f.write(html)
+
+path = "kernel/process"
+data = get_json_data(get_json_path(path))
+template = get_template()
+repdict = generate_repdict(data)
+html = generate_html(template, repdict)
+write_html(get_output_path(path), html)
